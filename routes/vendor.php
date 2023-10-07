201bd8facf5918cdba583bd0b21dda7ae7b813ed
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-10-07 23:59:10 +0300

    Seprate Route Files for Admin and Vendor

---
+++ routes/vendor.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\VendorController;
+
+/* Vendor Route  */
+Route::get('dashboard',[VendorController::class,'dashboard'])->name('dashboard');
