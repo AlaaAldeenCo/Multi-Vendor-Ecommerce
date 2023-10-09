279fbc2dd0905c4db413f4aaecb58d2af4486c7f
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-10-10 00:22:45 +0300

    Update Admin profile (Name and Email)

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 
 /* Admin Route  */
 Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+/*     Profile Routes     */
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
