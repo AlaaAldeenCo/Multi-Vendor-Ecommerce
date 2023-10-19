179c55eebfe0ec312948460a01a2b4bf90e1680e
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-10-19 22:40:54 +0300

    Create SubCategory and Make CRUD Operations on It

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 
 /* Admin Route  */
 Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -17,6 +18,10 @@ Route::post('profile/update/password', [ProfileController::class, 'updatePasswor
 /* Slider Routes */
 Route::resource('slider', SliderController::class);
 
+/* Category Route */
 Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
 Route::resource('category', CategoryController::class);
 
+/* SubCategory Route */
+Route::put('subcategory/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category', SubCategoryController::class);
