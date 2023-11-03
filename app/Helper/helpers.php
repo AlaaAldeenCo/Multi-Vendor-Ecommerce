23f7466e3db29eb41ad137df78d8fe35e5e3cec2
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-11-03 17:40:46 +0300

    Create Brand And Make Its CRUD Operations

---
+++ app/Helper/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+// Make Sidebar item active
+
+function setActive(array $route){
+    if(is_array($route)){
+        foreach($route as $r){
+            if(request()->routeIs($r)){
+                return 'active';
+            }
+        }
+    }
+}
