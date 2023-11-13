34e7a0e51c65870da3b8419517aff029f9f9893d
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-11-13 22:55:55 +0300

    Show Flash Sale Item In Home Page

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -11,3 +11,50 @@ function setActive(array $route){
         }
     }
 }
+
+/* Check If The Product Have Discount */
+
+function checkDiscount($product)
+{
+    $currentDate = date('Y-m-d');
+    if($product->offer_price >0 && $product->offer_start_date <= $currentDate && $currentDate <= $product->offer_end_date )
+    {
+        return true;
+    }
+    return false;
+}
+
+
+/* Calculate Discount in percernt */
+
+function calculateDiscountPercent($originalPrice, $discountPrice)
+{
+    $discountAmount = $originalPrice - $discountPrice;
+    $discountPercent = ($discountAmount / $originalPrice) * 100;
+    return $discountPercent;
+}
+
+/* Check The Product Type */
+
+function productType($type)
+{
+    switch ($type) {
+        case 'new_arrival':
+            return 'New';
+            break;
+        case 'featured_product':
+            return 'Featured';
+            break;
+        case 'top_product':
+            return 'Top';
+            break;
+
+        case 'best_product':
+            return 'Best';
+            break;
+
+        default:
+            return '';
+            break;
+    }
+}
--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\FlashSale;
+use App\Models\FlashSaleItem;
 class HomeController extends Controller
 {
     public function index()
     {
         $flashSaleDate = FlashSale::first();
+        $flashSaleItems = FlashSaleItem::where('show_at_home', 1)->where('status', 1)->get();
 
-        return view('frontend.home.home', compact ('flashSaleDate'));
+        return view('frontend.home.home', compact ('flashSaleDate','flashSaleItems'));
     }
 }
