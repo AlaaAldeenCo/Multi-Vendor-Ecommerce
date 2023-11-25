0b500a13268529f7cd9f83bcd0821cf48ef9c846
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-11-25 09:05:23 +0300

    Change Order Status Using Ajax Request

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -280,14 +280,30 @@
                     </div>
                     <div class="row mt-4">
                       <div class="col-lg-8">
-                        {{-- <div class="section-title">Payment Method</div>
-                        <p class="section-lead">The payment method that we provide is to make it easier for you to pay invoices.</p>
-                        <div class="images">
-                          <img src="assets/img/visa.png" alt="visa">
-                          <img src="assets/img/jcb.png" alt="jcb">
-                          <img src="assets/img/mastercard.png" alt="mastercard">
-                          <img src="assets/img/paypal.png" alt="paypal">
-                        </div> --}}
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="">Payment status</label>
+
+                                <select name="" id="payment_status" class="form-control" data-id="{{$order->id}}">
+                                    <option {{$order->payment_status === 0 ? 'selected': ''}} value="0">Pending</option>
+                                    <option {{$order->payment_status === 1 ? 'selected': ''}} value="1">Completed</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="">Order Status</label>
+                                <select name="order_status" class="form-control" id="order_status" data-id="{{$order->id}}">
+
+                                    @foreach (config('order_status.order_status_admin') as $key => $orderStatus )
+
+                                        <option {{$order->order_status === $key ? 'selected' : ''}} value = {{$key}} >{{$orderStatus['status']}}</option>
+
+                                    @endforeach
+
+
+                                </select>
+                            </div>
+                        </div>
                       </div>
                       <div class="col-lg-4 text-right">
                         <div class="invoice-detail-item">
@@ -327,5 +343,40 @@
 @endsection
 
 @push('scripts')
+
+    <script>
+
+        $(document).ready(function(){
+            $('#order_status').on('change', function(){
+                let status = $(this).val();
+                let id = $(this).data('id');
+                $.ajax({
+                    method: 'GET',
+                    url: '{{route("admin.order.status")}}',
+                    data:
+                    {
+                        status: status,
+                        id: id
+                    },
+
+                    success: function(data)
+                    {
+                        if(data.status === 'success')
+                        {
+                            toastr.success(data.message);
+                        }
+                    },
+
+                    error: function(data)
+                    {
+                        console.log(data);
+                    }
+
+                })
+            })
+        })
+
+    </script>
+
 
 @endpush
